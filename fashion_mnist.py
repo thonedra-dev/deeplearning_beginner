da0c5f5f6f0d79e3fe6861dbcf4c0e4bedcf1d77
thonedra-dev <thonedra.dev@gmail.com>
2025-12-03 19:15:37 +0800

    ..

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -27,3 +27,14 @@ y_test  = to_categorical(y_test, 10)
 
 print(f"New shape of X_train is {X_train.shape}")
 print(f"New shape of X_test is {X_test.shape}")
+
+from tensorflow.keras import models, layers
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(10, activation='softmax'))
+
+model.summary()
